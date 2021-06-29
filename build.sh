1cbfa19464bcae368e21bb338c3446c62b3ba386
zouyonghao <yonghaoz1994@gmail.com>
2021-06-29 03:26:07 +0000

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-sudo apt install npm
-sudo apt install coffeescript
-npm install -g cnpm --registry=https://registry.npm.taobao.org
-sudo apt install curl
+sudo apt install npm -y
+sudo apt install coffeescript -y
+sudo apt install curl -y
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g cnpm --registry=https://registry.npm.taobao.org
