88b2342cbd1c9b3c34b84043419352dc12694db6
zouyonghao <yonghaoz1994@gmail.com>
2021-09-16 13:21:10 +0000

    update build script

--- build.sh
+++ build.sh
@@ -5,9 +5,7 @@ curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g cnpm --registry=https://registry.npm.taobao.org
 sudo cnpm install -g browserify@13.1.0
-sudo apt install libcurl4-openssl-dev
-sudo apt install zlib1g-dev
-sudo apt install libssl-dev
+sudo apt install libcurl4-openssl-dev zlib1g-dev libssl-dev -y
 cp ~/distributed-system-test/rethinkdb_test/bin/compiler-config.json /tmp
 ./configure --with-system-malloc CXX=/home/zyh/distributed-system-test/build/fuzz/default_compiler++
 make
