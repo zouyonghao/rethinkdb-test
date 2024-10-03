9bc36a7aca002c5d3eca093e881036a003a50135
Yonghao Zou <yonghaoz1994@gmail.com>
2024-10-03 21:36:37 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,13 +20,7 @@ sudo npm install -g browserify@13.1.0
 sudo apt install libcurl4-openssl-dev -y
 sudo apt install zlib1g-dev -y
 sudo apt install libssl-dev -y
-if python --version 2>&1 | grep 3; then 
-	echo "set default python to version 2"
- 	sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1
-fi
 sudo apt install libcurl4-openssl-dev zlib1g-dev libssl-dev -y
-./configure --with-system-malloc --allow-fetch CXX=clang++-9 CXXFLAGS="-fsanitize=address" LDFLAGS="-fsanitize=address"
-make -j$(nproc)
 
-echo "set default python to version 3"
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+PYTHON=/usr/bin/python2 ./configure --with-system-malloc --allow-fetch CXX=clang++-9 CXXFLAGS="-fsanitize=address" LDFLAGS="-fsanitize=address"
+make -j$(nproc)
